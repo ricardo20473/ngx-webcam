026746ec065826cbb7e02c79921bec27b8819dc7
Ricardo Omaña <ricardo20473@gmail.com>
2022-09-16 20:23:59 -0400

    fix index

---
+++ src/app/modules/webcam/index.ts
@@ -0,0 +1,5 @@
+export * from "./webcam/webcam.component";
+export * from "./util/webcam.util";
+export * from "./domain/webcam-image";
+export * from "./domain/webcam-init-error";
+export * from "./domain/webcam-mirror-properties";
