ac29401f9422c30cb0127b9dfec4495bb38a2715
Ricardo Omaña <ricardo20473@gmail.com>
2022-09-16 20:48:37 -0400

    add lib

---
+++ projects/ngx-custom-webcam/src/public-api.ts
@@ -0,0 +1,10 @@
+/*
+ * Public API Surface of ngx-custom-webcam
+ */
+
+export * from "./lib/webcam.module";
+export * from "./lib/webcam/webcam.component";
+export * from "./lib/util/webcam.util";
+export * from "./lib/domain/webcam-image";
+export * from "./lib/domain/webcam-init-error";
+export * from "./lib/domain/webcam-mirror-properties";
--- src/app/modules/webcam/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./webcam/webcam.component";
-export * from "./util/webcam.util";
-export * from "./domain/webcam-image";
-export * from "./domain/webcam-init-error";
-export * from "./domain/webcam-mirror-properties";
